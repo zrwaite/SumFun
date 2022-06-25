00e3f7a31128450fcf503692db73dc7da5eb63f5
Greyson Martyn <greysonmartyn@gmail.com>
2022-06-25 08:37:54 -0400

    edit home view

--- FreyHacksApp/App.tsx
+++ FreyHacksApp/App.tsx
@@ -78,6 +78,8 @@ export default function App() {
 			<Stack.Screen name="Event" component={EventView} />
 			<Stack.Screen name="Friends" component={FriendsView} />
 			<Stack.Screen name="Friend" component={FriendView} />
+			<Stack.Screen name="Home" component={HomeView} />
+
           </Stack.Navigator>
         </NavigationContainer>
       )}
