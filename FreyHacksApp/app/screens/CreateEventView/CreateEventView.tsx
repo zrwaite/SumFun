434d8a240a97e7164674241ecc49d68a4bedd0cc
Kellan MacDonald <kmac1792@gmail.com>
2022-06-26 04:50:14 -0400

    Updated event creation

--- FreyHacksApp/app/screens/CreateEventView/CreateEventView.tsx
+++ FreyHacksApp/app/screens/CreateEventView/CreateEventView.tsx
@@ -1,21 +1,29 @@
-import { StyleSheet, View, Image, Text, TouchableOpacity, Alert } from 'react-native'
-const logoImage = require('../../assets/icon.png')
+import { StyleSheet, View, Image, Text, TouchableOpacity, Alert, TextInput, ScrollView } from 'react-native'
+const logoImage = require('../../assets/SumFun.png')
 const addImage = require('../../assets/add.png')
 import { useContext } from 'react'
+import { useState } from 'react'
 import { COLORS } from '../../settings'
 import { ZacButton } from '../../components/ZacButton'
 import { UserContext } from '../../../contexts'
 import { client } from '../../../client'
 import { CREATE_EVENT } from './queries'
 import { StackActions } from '@react-navigation/native'
+import RNPickerSelect from "react-native-picker-select";
+const Picker = RNPickerSelect as any;
 
 export const CreateEventView = ({ navigation }: { navigation: any }) => {
 	const { user, setUser } = useContext(UserContext)
+	const [name, setName] = useState('')
+	const [start_time, setStartTime] = useState(0)
+	const [end_time, setEndTime] = useState(0)
+	const [lat, setLat] = useState(0)
+	const [lon, setLon] = useState(0)
 
-	const tryCreateActivity = async () => {
+	const tryCreateEvent = async () => {
 		const response = await client.mutate({
 			mutation: CREATE_EVENT,
-			variables: { },
+			variables: {username: user?.username, name, start_time, end_time, lat, lon }
 		})
 		if (!response.errors) {
 			const data = response.data
@@ -28,21 +36,21 @@ export const CreateEventView = ({ navigation }: { navigation: any }) => {
 	}
 
 	return (
-		<View style={styles.container}>
-			<View style={styles.header}>
-				<Text style={styles.headerText}>Welcome, {user?.username}</Text>
-			</View>
-			<TouchableOpacity style={styles.clickSection} onPress={() => navigation.navigate('Settings')}>
-				<Image
-					source={addImage}
-					style={{
-						height: 30,
-						width: 30,
-						marginRight: 20,
-					}}/>
-				<Text style={styles.clickSectionText}>Create New Event</Text>
-			</TouchableOpacity>
-		</View>
+		<ScrollView style={styles.container} contentContainerStyle={{alignItems: 'center', justifyContent: 'flex-start',}}>	
+		    <View style={styles.header}>
+				<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.headerText}>Create Event</Text>
+				</View>
+					<TextInput style={styles.textInput} placeholder="Name" placeholderTextColor={COLORS.green} onChangeText={(newName) => setName(newName)} />
+					<TextInput style={styles.textInput} keyboardType={'numeric'} placeholder="Start Time" placeholderTextColor={COLORS.green} onChangeText={(newStartTime) => setStartTime(parseInt(newStartTime))} />
+					<TextInput style={styles.textInput} keyboardType={'numeric'} placeholder="End Time" placeholderTextColor={COLORS.green} onChangeText={(newEndTime) => setEndTime(parseInt(newEndTime))} />
+					<TextInput style={styles.textInput} keyboardType={'numeric'} placeholder="Lat" placeholderTextColor={COLORS.green} onChangeText={(newLat) => setLat(parseInt(newLat))} />
+					<TextInput style={styles.textInput} keyboardType={'numeric'} placeholder="Lon" placeholderTextColor={COLORS.green} onChangeText={(newLon) => setLon(parseInt(newLon))} />
+				</View>
+			</View>	
+		<ZacButton style={{ marginTop: 30 }} onPress={tryCreateEvent} color={COLORS.blue} text={'Create Event +'}/>
+		</ScrollView>
 	)
 }
 
@@ -57,13 +65,20 @@ const styles = StyleSheet.create({
 	headerText: {
 		color: 'white',
 		fontSize: 25,
-		textAlign: 'left',
+		textAlign: 'center',
+	},
+	textInput: {
+		height: 50,
+		textAlign: 'center',
+		width: '80%',
+		backgroundColor: 'white',
+		borderRadius: 20,
+		margin: 10,
+		fontSize: 20,
 	},
 	container: {
 		flex: 1,
-		backgroundColor: COLORS.lightgrey,
-		alignItems: 'center',
-		justifyContent: 'flex-start',
+		backgroundColor: COLORS.green,
 	},
 	clickSection: {
 		backgroundColor: 'white',
@@ -78,5 +93,13 @@ const styles = StyleSheet.create({
 	clickSectionText: {
 		color: 'black',
 		fontSize: 25,
+	},
+	picker: {
+		backgroundColor: 'white',
+		margin: 10,
+		width: '80%',
+		padding: 15,
+		textAlign: 'center',
+		borderRadius: 20,
 	},
 })
